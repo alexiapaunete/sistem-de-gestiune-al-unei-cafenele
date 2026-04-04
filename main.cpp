262eaae0292d19a5f4c02e80275d920c00934799
Ale <alexia.paunete@gmail.com>
2026-04-04 19:33:22 +0300

    tema 1

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@ class Ingredient {
 public:
     Ingredient(std::string n = "", double c = 0) : nume(n), cantitate(c) {}
 
-    // Operatorul << cerut pentru compunere
     friend std::ostream& operator<<(std::ostream& os, const Ingredient& i) {
         os << i.nume << ": " << i.cantitate;
         return os;
@@ -34,17 +33,12 @@ public:
 };
 
 class Produs {
-    Reteta reteta; // Compunere
+    Reteta reteta;
     double pret;
-    static int TVA; // Un mic plus pentru complexitate
-
+    static int TVA;
 public:
-    // Constructor cu parametri
     Produs(const Reteta& r, double p) : reteta(r), pret(p) {}
 
-    // Regula celor 3 (Rule of Three) implementata pe o clasa (ex: Produs sau Cafenea)
-    // Daca folosim std::vector si std::string, compilatorul le genereaza bine,
-    // dar cerinta cere sa le scrii tu explicit pentru o clasa.
     Produs(const Produs& other) : reteta(other.reteta), pret(other.pret) {}
 
     Produs& operator=(const Produs& other) {
@@ -55,9 +49,8 @@ public:
         return *this;
     }
 
-    ~Produs() {} // Destructor
+    ~Produs() {}
 
-    // Functie netriviala 1: Calcul pret cu reducere si taxe
     double calculeazaPretFinal(double discount) const {
         return (pret - (pret * discount / 100)) * 1.19;
     }
